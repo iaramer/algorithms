b84f386eaab2f5656e7c49f69d0d248d17542263
iaramer <yaramer95@gmail.com>
2021-07-20 12:13:43 +0300

    problem a

--- python/mipt_contest/contest/problems/A/problem_a.py
+++ python/mipt_contest/contest/problems/A/problem_a.py
@@ -1,11 +1,27 @@
+import numpy as np
 
 
-def get_trace(mx):
-    pass
+def get_trace(mat):
+    mx = np.array(mat)
+    return np.trace(mx)
 
+
+# if __name__ == '__main__':
+#     import sys
+#     sys.stdin = open('input001.txt', 'r')
+#     inp = list()
+#     for i in range(int(input())):
+#         inp.append(tuple(map(int, input().split())))
+#     assert get_trace(inp) == -137
+#
+#     sys.stdin = open('input002.txt', 'r')
+#     inp = list()
+#     for i in range(int(input())):
+#         inp.append(tuple(map(int, input().split())))
+#     assert get_trace(inp) == -63
+#     print('GJ!')
 
 inp = list()
 for i in range(int(input())):
     inp.append(tuple(map(int, input().split())))
-
 print(get_trace(inp))
